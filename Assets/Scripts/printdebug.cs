9bf7af88e5f0584665bd85fb5552ea6b065141a1
Daniel Lanfear <lanfeadb@ececs.udmercy.edu>
2022-03-02 10:54:08 -0500

    working toggle ray on pc

---
+++ Assets/Scripts/printdebug.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class printdebug : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    public void click()
+    {
+        Debug.Log("Button Clicked");
+    }
+}
